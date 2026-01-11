5063cd34689f88bf6f3d6ef54d8cad10f27e3146
gabrieltacosta <gabriel.tuka@gmail.com>
2026-01-11 11:46:11 -0300

    fix: prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,10 @@
-import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const prisma = new PrismaClient({ adapter: pool });
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-  });
+const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
