9fd4874d59ce1b614e4df07209862217f918c743
gabrieltacosta <gabriel.tuka@gmail.com>
2026-01-25 11:00:53 -0300

    update setup

--- src/app/dashboard/groups/new/actions.ts
+++ src/app/dashboard/groups/new/actions.ts
@@ -154,7 +154,7 @@ async function sendEmailToParticipants(
     const assignedName = participant.drawnParticipant?.name ?? "Amigo secreto não encontrado";
     
     return {
-      from: `Amigo Secreto ${groupName} <${process.env.EMAIL_SENDER_ADDRESS}>`,
+      from: `Amigo Secreto ${groupName} <${process.env.EMAIL_SENDER_NAME}>`,
       to: participant.email as string,
       subject: `Sorteio de amigo secreto - ${groupName}`,
       html: `<p>Você está participando do amigo secreto do grupo "${groupName}".<br /><br />
