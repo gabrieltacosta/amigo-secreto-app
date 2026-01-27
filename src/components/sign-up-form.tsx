5daa570a9ec0162d6e733725158ec9c19883a147
gabrieltacosta <gabriel.tuka@gmail.com>
2026-01-27 12:44:30 -0300

    update: signup form

--- src/components/sign-up-form.tsx
+++ src/components/sign-up-form.tsx
@@ -14,7 +14,7 @@ import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import Image from "next/image";
 import { Eye, EyeOff, Loader2, X } from "lucide-react";
-import { signUp } from "@/lib/auth-client";
+import { signIn, signUp } from "@/lib/auth-client";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import {
@@ -29,6 +29,7 @@ import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 const SignUpSchema = z
   .object({
@@ -100,7 +101,7 @@ export default function SignUp() {
         onError: (ctx) => {
           toast.error(
             ctx.error.message === "User already exists. Use another email." &&
-              "Email já cadastrado!"
+            "Email já cadastrado!"
           );
           setLoading(false);
         },
@@ -112,6 +113,28 @@ export default function SignUp() {
       },
     });
   };
+
+  const handleSignInWithGoogle = async () => {
+    await signIn.social(
+      {
+        provider: "google",
+        callbackURL: "/dashboard"
+      },
+      {
+        onRequest: () => {
+          setLoading(true);
+        },
+        onSuccess: () => {
+          setLoading(false);
+          toast.success("Redirecionando para login com Google...");
+        },
+        onError: () => {
+          setLoading(false);
+          toast.error("Erro ao realizar login. Verifique suas credenciais.");
+        },
+      },
+    );
+  }
 
   return (
     <Card className="z-50 rounded-md rounded-t-none max-w-md">
@@ -293,9 +316,31 @@ export default function SignUp() {
             </div>
           </form>
         </Form>
-        <Link href="/login" className="mt-4 inline-block text-sm underline">
+        <Link href="/login" className="my-4 inline-block text-sm underline">
           Já tem uma conta? Faça login
         </Link>
+        <div className={cn(
+          "w-full gap-2 flex items-center",
+          "justify-between flex-col"
+        )}>
+
+          <Button
+            variant="outline"
+            className={cn(
+              "w-full gap-2"
+            )}
+            disabled={loading}
+            onClick={handleSignInWithGoogle}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 262">
+              <path fill="#4285F4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"></path>
+              <path fill="#34A853" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"></path>
+              <path fill="#FBBC05" d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z"></path>
+              <path fill="#EB4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"></path>
+            </svg>
+            Entrar com Google
+          </Button>
+        </div>
       </CardContent>
       <CardFooter>
         <div className="flex justify-center w-full border-t py-4">
