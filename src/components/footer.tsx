86ac13709f35f91c916ce39da52d77c135168cc6
gabrieltacosta <gabriel.tuka@gmail.com>
2026-01-27 11:53:51 -0300

    update:footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <div className="flex flex-col w-full items-center justify-center">
             <div className="flex container justify-center mx-auto p-4">
-                <span className="text-sm italic">&copy;{new Date().getFullYear()} | Amigo Secreto - Todos os direitos reservados.</span>
+                <span className="text-xs md:text-sm italic">&copy;{new Date().getFullYear()} | Amigo Secreto - Todos os direitos reservados.</span>
             </div>
         </div>
     )
