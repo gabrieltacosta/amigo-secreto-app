d645ec4fb49a62c55b4778ffac0d9bde861d4e24
gabrieltacosta <gabriel.tuka@gmail.com>
2026-02-10 18:04:13 -0300

    chore: manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,19 +1,20 @@
-import type { MetadataRoute } from 'next';
+import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Amigo Secreto Simples',
-    short_name: 'Amigo Secreto',
-    description: 'Crie sorteios de amigo secreto online, envie convites por e‑mail e revele os sorteados com segurança.',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#ec003f',
+    name: "Amigo Secreto Simples",
+    short_name: "Amigo Secreto",
+    description:
+      "Crie sorteios de amigo secreto online, envie convites por e‑mail e revele os sorteados com segurança.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#e4e4e7",
+    theme_color: "#ec003f",
     icons: [
       {
-        src: '/gift1.png',
-        sizes: '192x192',
-        type: 'image/png',
+        src: "/gift2.png",
+        sizes: "182x182",
+        type: "image/png",
       },
     ],
   };
